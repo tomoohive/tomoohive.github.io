819a69d21f3fd474d05b8acefd803827359cb3db
tomoohive <graphelate@gmail.com>
2020-01-25 15:13:38 +1300

    initialize

---
+++ src/components/demand.svelte
@@ -0,0 +1,16 @@
+<style>
+.demand{
+    font-family: 'Press Start 2P', cursive;
+    font-size: 6px;
+    position: absolute;
+    top: calc(50% + 170px);
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%);
+    z-index: 999;
+}
+</style>
+
+<div class='demand'>
+please click/tap aroud this page.
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>tomoohive Page</title>
+</svelte:head>
+
+<script>
+  import Person from '../components/person.svelte'
+  import Balloon from '../components/balloon.svelte'
+  import Title from '../components/title.svelte'
+  import Demand from '../components/demand.svelte'
+</script>
+
+<Title />
+<Person />
+<Balloon comment='Tomoyasu Futaba' top='-130px' left='90px'/>
+<Balloon comment='12th Oct 1997' top='-80px' left='-100px'/>
+<Balloon comment='Aim to Engineer' top='50px' left='90px'/>
+<Balloon comment='using Svelte' top='70px' left='-90px'/>
+<Demand />
